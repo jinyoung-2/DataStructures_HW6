2052caa53c9d2a607a9bc3b3b18598e9f9fd7190
brenda <brenda524@naver.com>
2021-04-10 02:02:05 +0900

    commit

--- singlylinkedlist.c
+++ singlylinkedlist.c
@@ -18,7 +18,7 @@ typedef struct Node {
 } listNode;  //노드 구조
 
 typedef struct Head {
-	struct Node* first;
+	struct Node* first;   //listNode* first;  //제출하기 전 나중에 삭제하기!!
 }headNode;  //헤드노드
 
 
@@ -141,10 +141,10 @@ int freeList(headNode* h){
 int insertFirst(headNode* h, int key) {
 
 	listNode* node = (listNode*)malloc(sizeof(listNode));  //key에 대한 노드하나 추가를 위한 동적할당 
-	node->key = key; //node에 key값 대입z
+	node->key = key; //node에 key값 대입
 
-	node->link = h->first;  //node의 link에 헤드노드의 first 대입 ----------->여기 check!(주석)
-	h->first = node;  //head노드를 node로 변경  Q. 이중포인터야? 아니면 단일포인터야? h가?
+	node->link = h->first;  //노드의 link에 헤드노드가 가리키는 주소 대입   //Q.확인 
+	h->first = node;  //head노드를 node로 변경  
 
 	return 0;
 }
@@ -152,29 +152,35 @@ int insertFirst(headNode* h, int key) {
 
 /* 리스트를 검색하여, 입력받은 key보다 큰값이 나오는 노드 바로 앞에 삽입 */
 //중간에 삽입--> 순차적으로!(오름차순으로 입력)
-/* Q. insertNode함수를 수행할 떄, 입력받은 key값보다 큰 값이 없는 경우에는 마지막에 노드를 추가하면 되나요?
-    A.*/
-
 int insertNode(headNode* h, int key) 
 {
-	listNode* node = (listNode*)malloc(sizeof(listNode));
-	listNode* prev=NULL;  //이전 노드 
-	listNode* cur=h->first;	 //현재 노드 
+	listNode* node = (listNode*)malloc(sizeof(listNode));   //새로운 노드 생성(동적할당)
+    node->key=key;
+    node->link=NULL;    //초기화
 
-	//삽입할 위치를 찾을 때
-	if((key>=(prev->key))&&(key<=(cur->key)))		//Q. 등호 맞나?
-	{
-		//cur 위치에 삽입
-		node->link=cur;	//cur의 위치를 node->link에 대입
-		prev->link=node;	//node의 위치를 prev->link에 대입
-		node->key=key;		//key값을 node의 데이터(key)에 대입 
-	}
-	//삽입할 위치 찾지 못할 때 prev, cur 위치 변경 
-	else
-	{
-		prev=cur;
+    //리스트 탐색을 위한 노드 포인터들 선언 
+	listNode* prev=NULL;        
+	listNode* cur=h->first;	    
+
+    //리스트에 노드가 존재하지 않을 때
+    if(h->first==NULL)  
+    {
+        h->first=node;      //첫번째 노드를 node로 설정   //Q. 맞나?  
+        return 0;
+    }
+    //리스트에 노드가 존재할 때
+    while(1)
+    {
+        if(key<=(cur->key))		//Q. 등호 맞나?
+            break;
+        //삽입할 위치 찾지 못할 때 prev, cur 위치 변경
+        prev=cur;
 		cur=cur->link;
-	}
+    }
+    //삽입할 위치를 찾을 때 -> cur 위치에 삽입
+	node->link=cur;	    //cur의 위치를 node->link에 대입
+	prev->link=node;	//node의 위치를 prev->link에 대입
+	
 	return 0;
 }
 
@@ -187,22 +193,30 @@ int insertNode(headNode* h, int key)
 //마지막에 삽입
 int insertLast(headNode* h, int key) 
 {
-	listNode* node = (listNode*)malloc(sizeof(listNode));  //key에 대한 노드 생성을 위한 동적할당 
+	listNode* node = (listNode*)malloc(sizeof(listNode));   //새로운 노드 생성(동적할당)
+    node->key=key;
+    node->link=NULL;    //초기화
+
+    //리스트 탐색을 위한 노드 포인터들 선언 
+	listNode* prev=NULL;        
+	listNode* cur=h->first;	 
+
+    //리스트에 노드가 존재하지 않을 때
+    if(h->first==NULL)
+    {
+        h->first=node;  //Q.
+        return 0;
+    }
+
+    //리스트에 노드가 1개 이상 존재할 때
+    while(cur!=NULL)
+    {
+        prev=cur; //prev는 cur 노드로 변경   
+		cur=cur->link;	//cur은 cur의 link가 가리키는 노드로 변경
+    }
+    //cur=NULL일 때(마지막 노드일 때)
+	prev->link=node;  //마지막 노드의 link에 생성한 node 연결(주소 대입)
 	
-	//Q. 마지막 노드는 어떻게 찾을까?
-	listNode* prev=NULL;  //이전 노드 
-	listNode* cur=h->first;	 //현재 노드 
-	if(cur->link==NULL)	//마지막 노드일 때
-	{
-		cur->link=node;  //마지막 노드의 link에 생성한 node 연결(주소 대입)
-		node->key=key;	//해당 key 대입
-		node->link=NULL;  //추가한 노드가 마지막에 위치하므로, 리스트의 끝을 알리고자 노드의 link에 NULL 대입
-	}
-	else	//마지막 노드가 아닐 때 다음 노드로 이동
-	{
-		prev=cur; //prev는 cur 노드로 변경   
-		cur=cur->link;	//cur은 cur의 link가 가리키는 노드로 변경 
-	}
 	return 0;
 }
 
@@ -223,7 +237,7 @@ int deleteFirst(headNode* h)
 	listNode* prev=NULL;  //이전 노드 
 	listNode* cur=h->first;	 //현재 노드 
 	prev=cur->link;
-	freeList(cur);
+	free(cur);
 	//혹시 cur도 설정해줘야 하나요? 안 해도 될거 같은데, 
 	return 0;
 }
@@ -261,7 +275,7 @@ int deleteLast(headNode* h)
 	if(cur->link==NULL)	//마지막 노드일 때
 	{
 		prev->link=NULL;
-		freeList(cur);   //Q. 가능?
+		free(cur);   //Q. 가능?
 	}
 	else	//마지막 노드가 아닐 때 다음 노드로 이동
 	{
